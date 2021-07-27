9771c3120cc018c6ce5018a287bc39b2188e3143
Rik van Duijn <ik@rikvanduijn.nl>
2021-07-27 17:08:02 +0200

    Cleanup

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,8 @@
 import { useRoute } from "@react-navigation/native"
 import * as React from "react"
-import { StyleSheet } from "react-native"
-import { AddDeckButton } from "../components/AddDeckButton"
 import { CurrentGame } from "../components/CurrentGame"
 
-import EditScreenInfo from "../components/EditScreenInfo"
 import { EmptyGame } from "../components/EmptyGame"
-import { Text, View } from "../components/Themed"
 import { Deck } from "../types"
 
 export default function TabTwoScreen() {
